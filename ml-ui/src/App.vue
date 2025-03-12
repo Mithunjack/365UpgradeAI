f8cb910269351e5f170a063e2376815f57cdf813
Mithunjack <mithund654@gmail.com>
2025-03-12 22:10:43 +0100

    Vue UI added

---
+++ ml-ui/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <PredictionForm />
+  </div>
+</template>
+
+<script>
+import PredictionForm from "./components/PredictionForm.vue";
+
+export default {
+  components: {
+    PredictionForm,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
